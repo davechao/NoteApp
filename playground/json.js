9f7c6860d09124b6994062caf4fdcf02519b3beb
dave_chao <dave@coolbitx.com>
2018-11-07 21:47:13 +0800

    first commit

---
+++ playground/json.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+var originalNote = {
+	title: "Some title",
+	body: "Some body"
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('note.json', originalNoteString);
+
+var noteString = fs.readFileSync('note.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
